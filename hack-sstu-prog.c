8188ce239ec2adb5ce5a281a4a85f785ee0e7af5
sed <igorkoneshov@mail.ru>
2021-11-21 19:58:21 +0400

    Добавил новую функцию max

--- hack-sstu-prog.c
+++ hack-sstu-prog.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <limits.h>
 #include "hack-sstu-prog.h"
 
 void sort(int *mass, int k)
@@ -93,4 +94,21 @@ int set_cur_array(int *b, int k)
         }
     }
     return k;
+}
+
+int max(int *mas, int k)
+{
+    /* Функция нахождения максимума в массиве Аналог функции max() в Python.
+    :max - Указатель на массив;
+    :k - Длинна массива;
+    */
+    long long int maxi = INT_MIN;
+    for (int i = 0; i < k; i++)
+    {
+        if (mas[i] > maxi)
+        {
+            maxi = mas[i];
+        }
+    }
+    return maxi;
 }
