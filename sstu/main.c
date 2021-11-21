95312594fa53b082aa16a8cdb2a2a0b6672bf5f6
Ildar <wilidon@bk.ru>
2021-11-21 10:27:03 +0400

    Initial Commit

---
+++ sstu/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  sstu
+//
+//  Created by Ильдар Рамазанов on 21.11.2021.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
