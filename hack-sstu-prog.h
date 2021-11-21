a59e5e3af1ac652e02aa33d8d486860e3b9e8276
sed <igorkoneshov@mail.ru>
2021-11-21 21:11:23 +0400

    Добавил функцию min и max в hack-sstu-prog.h

--- hack-sstu-prog.h
+++ hack-sstu-prog.h
@@ -5,5 +5,6 @@ void sort(int *mass, int k);
 int count_s(char *mass, char ch);
 int *set_new_array(int *mass, int k);
 int set_cur_array(int *b, int k);
-
+int max(int *mas, int k);
+int min(int *mas, int k);
 #endif
